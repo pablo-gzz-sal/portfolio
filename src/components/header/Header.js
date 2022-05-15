b8876e0871b32f929352fc7f42b2209082e051d2
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2022-05-15 11:22:50 +0200

    changes in header. Img and animations creation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,36 +1,46 @@
 import React from "react";
 import "./Header.css";
 import DarkMode from "../darkMode/DarkMode";
-import Language from "../language/Language"
-import logo from "../../images/logo.png"
+import Language from "../language/Language";
+import logo from "../../images/logo_transparent.png";
+import bgImg from "../../images/bgImg.jpg";
 
-function Header({darkMode, setDarkMode, language, setLanguage}) {
+function Header({ darkMode, setDarkMode, language, setLanguage }) {
   return (
-    <header className="header">
-      <div className="logo">
+    <section className="homePage">
+      <img src={bgImg} alt="homepage Image" className="bgImg" />
+      <h1 className="titleHP">Hi! I'm Pablo and I'm a front-end developer</h1>
+      <span className="arrowHP">
+        <a href="#aboutme" target="_self">
+        <i class="fal fa-arrow-down arrow fa-2x"></i>
+        </a>
+      </span>
+      <header className="header">
+        <div className="logo">
           <img src={logo} alt="logo" className="logoImg" />
-      </div>
-      <div className="menuBar">
-        <ul class="list">
-          <li class="list_item">
-            <a href="#episodios">About me</a>
-          </li>
-          <li class="list_item">
-            <a href="#nosotros">Skills</a>
-          </li>
-          <li class="list_item">
-            <a href="#entrevistas">Projects</a>
-          </li>
-          <li class="list_item">
-            <a href="#topicos">Contact me</a>
-          </li>
-        </ul>
-      </div>
-      <div className="darkAndLan">
+        </div>
+        <div className="menuBar">
+          <ul class="list">
+            <li class="list_item">
+              <a href="#episodios">About me</a>
+            </li>
+            <li class="list_item">
+              <a href="#nosotros">Skills</a>
+            </li>
+            <li class="list_item">
+              <a href="#entrevistas">Projects</a>
+            </li>
+            <li class="list_item">
+              <a href="#topicos">Contact me</a>
+            </li>
+          </ul>
+        </div>
+        <div className="darkAndLan">
           <DarkMode darkMode={darkMode} setDarkMode={setDarkMode} />
           <Language language={language} setLanguage={setLanguage} />
-      </div>
-    </header>
+        </div>
+      </header>
+    </section>
   );
 }
 
