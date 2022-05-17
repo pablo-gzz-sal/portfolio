a2efdb46e7c98714450898ae2e3fbcfd706e7c12
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2022-05-17 14:39:48 +0200

    skills update

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Skills.css";
 import firebase from "../../svg/firebase.svg";
+import woocommerce from "../../images/woocommerce.png";
+import oxygen from "../../images/oxygenbuilder-logo.jpg"
 
 function Skills() {
   return (
     <section className="skills">
       <h2 className="titleSkills">Technical Skills</h2>
       <div className="columnsSkills">
-        <span className="column">
+        <span className="column column1st">
           <h3 className="titleColumn"></h3>
           <ul className="listColumn">
             <div className="textAndIcon">
@@ -53,25 +55,25 @@ function Skills() {
           <ul className="listColumn">
             <div className="textAndIcon">
               <li className="textSkills">Wordpress</li>
-              <li className="iconSkills iconFirebase">
+              <li className="iconSkills iconWP">
                 <i class="fab fa-wordpress fa-3x"></i>
               </li>
             </div>
             <div className="textAndIcon">
               <li className="textSkills">Oxygen</li>
               <li className="iconSkills iconFirebase">
-                <img src={firebase} alt="firebase logo" />
+                <img src={oxygen} alt="firebase logo" />
               </li>
             </div>
             <div className="textAndIcon">
               <li className="textSkills">Woocommerce</li>
-              <li className="iconSkills iconFirebase">
-                <img src={firebase} alt="firebase logo" />
+              <li className="iconSkills iconWoo">
+                <img src={woocommerce} alt="woocommerce logo" />
               </li>
             </div>
             <div className="textAndIcon">
               <li className="textSkills">Wix</li>
-              <li className="iconSkills iconFirebase">
+              <li className="iconSkills iconWix">
                 <i class="fab fa-wix fa-3x"></i>
               </li>
             </div>
